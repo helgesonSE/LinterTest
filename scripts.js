487bb78e0c76f4d42224f082e30835d1713db9f3
helgesonSE <140814418+helgesonSE@users.noreply.github.com>
2024-08-27 09:50:46 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 const x = 5
 const y = 7
 
-function double(x) { return x * 2 }
+function double (x) { return x * 2 }
 
 console.print('Dubbelt av 5 är ' + double(x))
 console.print('Dubbelt av 7 är ' + double(y))
